5364bf8552207937f4c06e34f96c5cdc220f51b0
Dylan Heermann <dylan.heermann@yahoo.com>
2017-11-15 14:33:08 -0500

    Added Migrations

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElevenNote.Models;
+using ElevenNote.Web.Data;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        public IEnumerable<NoteListItemModel> GetNotes()
+        {
+            using (var ctx = new ElevenNoteDbContext())
+            {
+                return
+                    ctx
+                        .Notes
+                        .Select(
+                            e => 
+                                new NoteListItemModel
+                                {
+                                NoteId = e.NoteId,
+                                Title= e.Title,
+                                CreatedUtc = e.CreatedUtc,
+                                ModifiedUtc = e.ModifiedUtc
+                                })
+
+                       .ToArray();
+
+            }
+        }
+    }
+}
