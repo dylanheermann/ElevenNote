3de0515d630ef35204608db00a0897925d5b61c6
Dylan Heermann <dylan.heermann@yahoo.com>
2017-11-16 16:29:15 -0500

    Added stars

--- ElevenNote.Data/NoteEntity.cs
+++ ElevenNote.Data/NoteEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -22,6 +23,11 @@ namespace ElevenNote.Data
         [Required]
 
         public string Title { get; set; }
+
+        
+
+        [DefaultValue(false)]
+        public bool IsStarred { get; set; }
 
         [MaxLength(500)]
 
