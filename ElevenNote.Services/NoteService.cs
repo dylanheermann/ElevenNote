658a626cce14f1882ae6898bbe01c8938c4348fe
Dylan Heermann <dylan.heermann@yahoo.com>
2017-11-16 10:40:12 -0500

    Connected detail page

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ElevenNote.Data;
 using ElevenNote.Models;
 using ElevenNote.Web.Data;
 
@@ -10,26 +11,82 @@ namespace ElevenNote.Services
 {
     public class NoteService
     {
+        private readonly Guid _userId;
+        private string v;
+
+        public Guid OwnerId { get; private set; }
+
+        public NoteService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+     
         public IEnumerable<NoteListItemModel> GetNotes()
         {
+
             using (var ctx = new ElevenNoteDbContext())
             {
                 return
                     ctx
                         .Notes
+                        .Where(e => OwnerId == _userId)
                         .Select(
-                            e => 
+                            e =>
                                 new NoteListItemModel
                                 {
-                                NoteId = e.NoteId,
-                                Title= e.Title,
-                                CreatedUtc = e.CreatedUtc,
-                                ModifiedUtc = e.ModifiedUtc
+                                    NoteId = e.NoteId,
+                                    Title = e.Title,
+                                    CreatedUtc = e.CreatedUtc,
+                                    ModifiedUtc = e.ModifiedUtc
                                 })
 
                        .ToArray();
 
             }
+        }
+        public bool CreateNote(NoteCreateModel model)
+        {
+            using (var ctx = new ElevenNoteDbContext())
+        {
+
+                var entity =
+                        new NoteEntity
+                        {
+                            OwnerId = _userId,
+                            Title = model.Title,
+                            Content = model.Content,
+                            CreatedUtc = DateTime.UtcNow
+                        };
+
+                ctx.Notes.Add(entity);
+
+               return ctx.SaveChanges() == 1;
+
+            }
+        }
+        NoteEntity entity;
+
+        public NoteDetailModel GetNoteById(int id)
+        {
+            using (var ctx = new ElevenNoteDbContext())
+            {
+                ctx
+                    .Notes
+                    .SingleOrDefault(e => e.NoteId == id && e.OwnerId == _userId);
+            }
+
+            if (entity == null) return new NoteDetailModel();
+
+            return
+                new NoteDetailModel
+                {
+                    NoteId = entity.NoteId,
+                    Title = entity.Title,
+                    Content = entity.Content,
+                    CreatedUtc = entity.CreatedUtc,
+                    ModifiedUtc = entity.ModifiedUtc
+                };
         }
     }
 }
